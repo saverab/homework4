79e8947cc5e115471194d02d01336c704d0a7850
saverab <43477887+saverab@users.noreply.github.com>
2018-10-07 11:21:32 -0700

    Create 10-circles.js

---
+++ 10-circles.js
@@ -0,0 +1,20 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+var x = []; // new empty array
+var y = []; // new empty array
+
+function draw() {
+  background(255);
+  noFill();
+
+  x.push(mouseX); // equivalent to append(x, mouseX)
+  y.push(mouseY); // equivalent to append(y, mouseY)
+
+  for (var i = 0; i < x.length/5.5; i = i + 1) {
+    ellipse(x[i], y[i], 1 + 5*(50 - i)); } 
+
+  x = x.slice(-50); // keep the last 50 x values
+  y = y.slice(-50); // keep the last 50 y values
+}
